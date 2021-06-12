1bc64dedc0507ddcbaa0adbcf330858504742b0c
Forwork89 <81515618+Forwork89@users.noreply.github.com>
2021-06-12 14:50:25 +0300

    Add files via upload
    zadanie 13.819

---
+++ задание 13.8.19.py
@@ -0,0 +1,14 @@
+tickets = int(input("Ввыберете количество билетов"))
+price = 0
+
+for visitor in range(tickets,):
+    age = int(input("Введите ваш возраст пожалйста:"))
+    if age < 18:
+        price += 0
+    elif 18 <= age < 25:
+        price += 990
+    elif age > 25:
+        price = 1390
+if tickets > 3:
+    price = price - (price * 0.1)
+print("Сумма к оплате", price)
