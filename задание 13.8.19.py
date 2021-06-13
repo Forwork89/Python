7396bfe305861f473baf41512274c66ab473e842
Forwork89 <81515618+Forwork89@users.noreply.github.com>
2021-06-13 20:42:37 +0300

    dz13
    Zdanie13

--- задание 13.8.19.py
+++ задание 13.8.19.py
@@ -8,7 +8,7 @@ for visitor in range(tickets,):
     elif 18 <= age < 25:
         price += 990
     elif age > 25:
-        price = 1390
+        price += 1390
 if tickets > 3:
     price = price - (price * 0.1)
 print("Сумма к оплате", price)
