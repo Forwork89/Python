ba36b5a4ba4c3dade10db5688a6acc01d9e58b6a
Forwork89 <81515618+Forwork89@users.noreply.github.com>
2021-06-29 19:03:06 +0300

    Add files via upload
    16.8.2

---
+++ Circle.py
@@ -0,0 +1,7 @@
+from circle_1 import Circle
+
+r_1 = Circle(5,4)
+p_1 = Circle(3,14)
+
+print(r_1.get_area())
+print(p_1.get_area())
---
+++ circle_1.py
@@ -0,0 +1,6 @@
+class Circle:
+    def __init__(self, r, p):
+        self.r = r
+        self.p = p
+    def get_area(self):
+        return (self.r * self.p) ** 2
