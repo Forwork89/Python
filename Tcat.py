c6ade4df66f087b38a3e8b737cfda5f25b426045
Forwork89 <81515618+Forwork89@users.noreply.github.com>
2021-06-29 11:00:07 +0300

    Add files via upload
    Cats 16.7.1

---
+++ Tcat.py
@@ -0,0 +1,13 @@
+from Cat import Cat
+
+c1 = Cat('baron', 'boy', 2)
+print('c1.name', c1.name)
+print('c1.gender', c1.gender)
+print('c1.age', c1.age)
+
+
+c2 = Cat('Sam', 'boy', 2)
+print('c2.name', c2.name)
+print('c2.gender', c2.gender)
+print('c2.age', c2.age)
+
